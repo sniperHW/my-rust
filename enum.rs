b34064483ab43e8a1e5d0973757680358250e011
sniperHW <huangweilook@21cn.com>
2015-01-30 14:42:52 +0800

    添加2.9节

--- enum.rs
+++ enum.rs
@@ -5,13 +5,19 @@ enum StringResult {
 
 fn respond(greeting: &str) -> StringResult {
     if greeting == "Hello" {
-        StringResult::StringOK("Good morning!".to_string())
+        StringResult::StringOK("Good morning!" . to_string())
     } else {
-        StringResult::ErrorReason("I didn't understand you!".to_string())
+        StringResult::ErrorReason("I didn't understand you!" . to_string())
     }
 }
 
 fn main() {
-	let ret = respond("Hello");
-
+	match respond("Hello") {
+        		StringResult::StringOK(n) => println!("{}", n),
+        		StringResult::ErrorReason(n) => println!("{}",n),
+    	}
+	match respond("How are you") {
+        		StringResult::StringOK(n) => println!("{}", n),
+        		StringResult::ErrorReason(n) => println!("{}",n),
+    	}    	
 }
