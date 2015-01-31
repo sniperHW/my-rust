f626857c9c8301cddaadebef7016531f163256e5
sniperHW <huangweilook@21cn.com>
2015-01-31 14:22:07 +0800

    添加一些文档资料

--- array.rs
+++ array.rs
@@ -1,3 +1,15 @@
+
+fn show_array(a2d: [[i32; 3]; 3],x: usize,y: usize) {
+	print!("show_array\n");
+	/*for j in 0..3 {
+		for i in 0..3 {
+			print!("{}",a2d[j][i]);
+		}
+		print!("\n");
+	}*/
+	println!("{}",a2d[y][x]);	
+}
+
 fn main() {
 	let a: [i32; 3] = [1, 2, 3];
 	//println!("{}",a[1]);
@@ -6,4 +18,13 @@ fn main() {
 	}
 	let names = ["Graydon", "Brian", "Niko"]; // names: [&str; 3]
 	println!("{},{},{}", names[0],names[1],names[2]);
+
+	let a2d: [[i32; 3]; 3] = [[1,2,3],[4,5,6],[7,8,9]];
+	/*for j in 0..3 {
+		for i in 0..3 {
+			print!("{}",a2d[j][i]);
+		}
+		print!("\n");
+	}*/
+	show_array(a2d,1,2);
 }
--- snail.rs
+++ snail.rs
@@ -1,37 +1,16 @@
-/*local maxx = 4
-local maxy = 3
-local final = maxx * maxy
+/*输出一个4X3矩阵,沿上下左右前进,每个格子都走一遍的走法数量,
+例如下面是一些合法的走法:
 
-local dir = {
-	up = function (x,y)
-		y = y - 1
-		if y < 1 then
-			return nil
-		end
-		return x,y
-	end, 
-	down = function (x,y)
-		y = y + 1
-		if y > maxy then
-			return nil
-		end
-		return x,y
-	end, 
-	left  = function (x,y)
-		x = x - 1
-		if x < 1 then
-			return nil
-		end
-		return x,y
-	end,
-	right = function (x,y)
-		x = x + 1
-		if x > maxx then
-			return nil
-		end
-		return x,y
-	end
-}*/
+1  2  3  4
+10 11 12 5 
+9  8  7  6		
+
+1 2 9 10
+4 3 8 11 
+5 6 7 12
+*/
+
+static mut total: u32 = 0;
 
 fn up(x: i32,y: i32) -> (bool,i32,i32) {
 	if y -1 < 0 {
@@ -42,7 +21,7 @@ fn up(x: i32,y: i32) -> (bool,i32,i32) {
 }
 
 fn down(x: i32,y: i32) -> (bool,i32,i32) {	
-	if y + 1 >= 4 {
+	if y + 1 >= 3 {
 		(false,0,0)
 	}else{
 		(true,x,y+1)
@@ -65,12 +44,44 @@ fn right(x: i32,y: i32) -> (bool,i32,i32) {
 	}
 }
 
+static DIR: [fn(i32,i32) -> (bool,i32,i32); 4] = [up, down,left,right];
+
+fn is_vaild(matrix: [[i32; 4]; 3],x: i32,y: i32) -> bool {
+	if x < 0 || y < 0  || x >= 4 || y >= 3 {
+		return false;
+	}
+	if matrix[y as usize][x as usize] > 0 {
+		return false;
+	}
+	return true;
+}
+	
+fn snail(mut matrix: [[i32; 4]; 3],mut c: i32,x: i32,y: i32) {
+	if is_vaild(matrix,x,y) {
+		c = c + 1;
+		matrix[y as usize][x as usize] = c;
+		if c == 12 {
+			unsafe { total = total + 1; }
+			return;
+		}
+		for i in 0..4 {
+			let (vaild,nextx,nexty) = DIR[i](x,y);
+			if vaild {
+				snail(matrix,c,nextx,nexty);
+			}
+		}		
+	}
+}
+
 
 fn main() {
-	let (ret,x,y) = up(1,2);
-	if ret {
-		println!("{},{}",x,y);
+	let matrix: [[i32; 4]; 3] = [[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+	for j in 0..3 {
+		for i in 0..4 {
+			snail(matrix,0,i,j);
+		}
 	}
+	unsafe{ println!("{}",total); }
 }
 
 
